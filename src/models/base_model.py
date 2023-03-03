957f7d4714bf4c413aa0e5cb0d4f35c4f6c22c4a
ac2393921 <ac2393921@gmail.com>
2023-03-03 23:37:35 +0900

    add: モデルインターフェースの追加

---
+++ src/models/base_model.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Optional, Union
+
+import numpy as np
+import pandas as pd
+
+
+class BaseRecommendModel(ABC):
+    def __init__(self):
+        self.name: str = "base_beverage_hotel_recommend"
+        self.params: Dict = {}
+        self.model = None
+
+    @abstractmethod
+    def reset_model(self, params: Optional[Dict] = None) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def train(
+        self,
+        x_train: Union[np.ndarray, pd.DataFrame],
+        y_train: Union[np.ndarray, pd.DataFrame],
+        x_test: Optional[Union[np.ndarray, pd.DataFrame]] = None,
+        y_test: Optional[Union[np.ndarray, pd.DataFrame]] = None,
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    def predict(
+        self, x: Union[np.ndarray, pd.DataFrame]
+    ) -> Union[np.ndarray, pd.DataFrame]:
+        raise NotImplementedError
